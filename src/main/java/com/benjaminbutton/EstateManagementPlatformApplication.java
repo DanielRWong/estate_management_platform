a9cb9348281034b1d3b375ce728d7705c49d3611
daniel <danielrodeowong@gmail.com>
2020-06-24 03:16:51 +0800

    '项目初始状态'

---
+++ src/main/java/com/benjaminbutton/EstateManagementPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.benjaminbutton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstateManagementPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstateManagementPlatformApplication.class, args);
+    }
+
+}
